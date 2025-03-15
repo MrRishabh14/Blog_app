cf0deec44a621d1504118b515d61073168216591
Rishabh Puthran <rishabhputhran142004@gmail.com>
2025-03-15 15:13:49 +0530

    env updated and functions

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 
 function App() {
-
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+  console.log(import.meta.env.VITE_APPWRITE_PROJECT_ID)
   return (
     <div className='bg-amber-300'>
       <h1>Welcome to the Blog App</h1>
+      <p>this is the blog website</p>
     </div>
   )
 }
